1702c95c86f652569085b259b76703a36970bdb5
ColtonBarnacle <colton.barnacle@students.fhu.edu>
2024-02-01 10:05:21 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 #REMOVE PASS AND FIX THE FUNCTION
 #change to test push and forks
-def sum_of_products(list1, list2):
-    pass
+def multiply_and_sum(series1, series2):
+    if len(series1) != len(series2):
+        raise ValueError
+
+    result = sum(x * y for x, y in zip(series1, series2))
+    return result
+
+def main():
+    series1_str = input("Enter the first series of integers: ")
+    series2_str = input("Enter the second series of integers: ")
+
+    series1 = [int(num) for num in series1_str]
+    series2 = [int(num) for num in series2_str]
+
+    result = multiply_and_sum(series1, series2)
+    print("The sum of multiplying corresponding items is:", result)
+        
 
 if __name__ == '__main__':
-   #REMOVE PASS AND YOUR CODE GOES HERE
-    pass
+    main()
