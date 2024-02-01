fedbf7a1976c3a7af8775d55795fd54fd0cbcfcb
ColtonBarnacle <colton.barnacle@students.fhu.edu>
2024-02-01 11:20:21 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 #change to test push and forks
 def multiply_and_sum(series1, series2):
     if len(series1) != len(series2):
-        return "Error: Lists must be the same length."
+        return "Error"
 
     result = sum(x * y for x, y in zip(series1, series2))
     return result
@@ -15,7 +15,7 @@ def main():
 
     # Calculate and display the result
     result = multiply_and_sum(series1, series2)
-    print("Output =", result)
+    print(result)
 
 if __name__ == "__main__":
     main()
