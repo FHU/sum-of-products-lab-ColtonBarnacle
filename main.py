8fba6d4f6151968be52bf3290755e8a924c088b3
ColtonBarnacle <colton.barnacle@students.fhu.edu>
2024-02-01 11:28:35 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,20 +2,19 @@
 #change to test push and forks
 def multiply_and_sum(series1, series2):
     if len(series1) != len(series2):
-        return "Error"
+        return "Error: Lists must be the same length."
 
     result = sum(x * y for x, y in zip(series1, series2))
     return result
 
 def main():
     # Input series from the user
-    series1 = [int(x) for x in input("Enter the first series of integers : ").split()]
-    series2 = [int(x) for x in input("Enter the first series of integers : ").split()]
-
+    series1 = [int(x) for x in input("Enter the first series of integers:").split()]
+    series2 = [int(x) for x in input("Enter the second series of integers: ").split()]
 
     # Calculate and display the result
     result = multiply_and_sum(series1, series2)
-    print(result)
+    print("Output =", result)
 
 if __name__ == "__main__":
     main()
